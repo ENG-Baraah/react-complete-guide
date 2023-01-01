d964fc3f9596fa66a41309f8b67e374cca6492a4
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-01-01 23:18:10 +0300

    Adding Expense Form Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -12,6 +13,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
